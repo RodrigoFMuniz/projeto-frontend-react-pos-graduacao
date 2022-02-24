f0c826ec0ccd842dc3ef264288074e1d22b1611e
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-02-24 01:46:12 -0300

    Peer dependency issue among npm>=7.x and @reach-router corrected

--- src/routers.js
+++ src/routers.js
@@ -1,9 +1,15 @@
-import Home from './views/home.jsx'
+import React from 'react'
 import { Router } from '@reach/router'
+
+//views
+import Home from './views/home'
+import Sobre from './views/sobre'
+
 const Routers = ()=>{
   return (
     <Router>
-    <Home path="/" />
+     <Home path="/" />
+     <Sobre path="sobre" />
     </Router>
   )
 
