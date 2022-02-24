3456109201ec4213119591cff7bd2c70a84fa89b
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-02-23 23:56:59 -0300

    Using reach-router instead of router-react-dom

---
+++ src/routers.js
@@ -0,0 +1,11 @@
+import Home from './views/home.jsx'
+import { Router } from '@reach/router'
+const Routers = ()=>{
+  return (
+    <Router>
+    <Home path="/" />
+    </Router>
+  )
+
+}
+export default Routers;
