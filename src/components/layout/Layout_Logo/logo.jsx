7b79f0a91db5e8a4ec9fdf0ea4efbe06d7e8b493
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-03-10 14:29:37 -0300

    Added styles to layout logo  component

--- src/components/layout/Layout_Logo/logo.jsx
+++ src/components/layout/Layout_Logo/logo.jsx
@@ -1,5 +1,5 @@
 import { FaWpforms } from '@react-icons/all-files/fa/FaWpforms'
-import styled from 'styled-components'
+import { Animate, LogoApp } from './logo.styles';
 import {Col} from 'reactstrap'
 
 const Logo = ()=>{
@@ -17,45 +17,3 @@ const Logo = ()=>{
   );
 }
 export default Logo
-
-const LogoApp = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content:center;
-  align-items: center;
-`
-const Animate = styled.div`
-  span {
-    color: #c59d10;
-    font-size: 2rem;
-    margin-right: 1rem;
-  }
-  span:nth-of-type(2) {
-    color: #1e2a39;
-    font-weight: 400;
-    font-size: 1.8rem;
-  }
-  overflow: hidden;
-  border-right: 0.1em solid #c59d10;
-  margin-top:1rem;
-  white-space: nowrap;
-  letter-spacing: 0.15em;
-  animation: typewriter 3.0s steps(25, end), blink-pipe 0.55s step-end infinite;
-  @keyframes typewriter {
-    from {
-      width: 0;
-    }
-    to {
-      width: 20%;
-    }
-  }
-  @keyframes blink-pipe {
-    from,
-    to {
-      border-color: transparent;
-    }
-    50% {
-      border-color: #c59d10;
-    }
-  }
-`;
